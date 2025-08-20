8df9b665d02d3cc03cb27636a7efe59c9e56d9ae
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-08-20 16:00:25 +0530

    First commit from VS Code

--- backendnew/api/urls.py
+++ backendnew/api/urls.py
@@ -6,10 +6,10 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('sailorslist/',views.sailorusersdetails.as_view(), name='sailors-list method: no '),
-    path('usercreation/',views.send_verification_email, name='user-creation method : post '),
-    path('sailoremails/',views.sailorlist.as_view(),name="sailor email method: no"),
-    path('sailorform/',views.create_sailor,name="sailor form method:post"),
+    path('sailorslist/',views.sailorusersdetails.as_view(), name='sailors-list method no '),
+    path('usercreation/',views.send_verification_email, name='user-creation method  post '),
+    path('sailoremails/',views.sailorlist.as_view(),name="sailor email method no"),
+    path('sailorform/',views.create_sailor,name="sailor form method post"),
     
     
 
--- backendnew/backendnew/settings.py
+++ backendnew/backendnew/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,6 +131,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [ BASE_DIR / "media" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
